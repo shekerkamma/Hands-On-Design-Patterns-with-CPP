6c510b204cec5d9445c089b58fe672853bc80236
Hans-J. Schmid <knock@myopendoor.de>
2019-07-05 09:34:33 +0200

    adds comments to not compiling code.

--- Chapter04-Swap-and-the-standard-template-library/std_swap_noncopyable.cpp
+++ Chapter04-Swap-and-the-standard-template-library/std_swap_noncopyable.cpp
@@ -1,21 +1,25 @@
 #include <utility>
 #include <iostream>
 
-class A {
-    public:
+class A
+{
+public:
     explicit A(int i) : i_(i) {}
-    A(const A& rhs) = delete;
-    A& operator=(const A& rhs) = delete;
+    A(const A &rhs) = delete;
+    A &operator=(const A &rhs) = delete;
     int i_;
 };
 
-
-int main() {
+int main()
+{
     A a1(1), a2(2);
     std::cout << "\nInitial:" << std::endl;
     std::cout << "a1: " << a1.i_ << " a2: " << a2.i_ << std::endl;
 
-    // std::swap(a1, a2);  // Not the best way
+#if 0
+    // Class A does not have a copy constructor nor a move constructor. Will not compile!
+    std::swap(a1, a2);  // Not the best way
     std::cout << "\nAfter swap:" << std::endl;
     std::cout << "a1: " << a1.i_ << " a2: " << a2.i_ << std::endl;
+#endif
 }
--- Chapter04-Swap-and-the-standard-template-library/std_swap_nonmovable.cpp
+++ Chapter04-Swap-and-the-standard-template-library/std_swap_nonmovable.cpp
@@ -1,23 +1,35 @@
 #include <utility>
 #include <iostream>
 
-class A {
-    public:
+class A
+{
+public:
     explicit A(int i) : i_(i) {}
-    A(const A& rhs) : i_(rhs.i_) { std::cout << "A(const A&)" << std::endl; }
-    A(const A&& rhs) = delete;
-    A& operator=(const A& rhs) { i_ = rhs.i_; std::cout << "A = A&" << std::endl; return *this; }
-    A& operator=(const A&& rhs) = delete;
+    A(const A &rhs) : i_(rhs.i_) { std::cout << "A(const A&)" << std::endl; }
+    A(const A &&rhs) = delete;
+    A &operator=(const A &rhs)
+    {
+        i_ = rhs.i_;
+        std::cout << "A = A&" << std::endl;
+        return *this;
+    }
+    A &operator=(const A &&rhs) = delete;
     int i_;
 };
 
-
-int main() {
+int main()
+{
     A a1(1), a2(2);
     std::cout << "\nInitial:" << std::endl;
     std::cout << "a1: " << a1.i_ << " a2: " << a2.i_ << std::endl;
 
-    // std::swap(a1, a2);  // Not the best way
+#if 0
+    // Note that if the class has copy operations declared and
+    // move operations declared as deleted, there is no automatic fallback to
+    // copying — that class is a non-movable type and std::swap() will not compile
+    // for it:
+    std::swap(a1, a2);
     std::cout << "\nAfter swap:" << std::endl;
     std::cout << "a1: " << a1.i_ << " a2: " << a2.i_ << std::endl;
+#endif
 }
