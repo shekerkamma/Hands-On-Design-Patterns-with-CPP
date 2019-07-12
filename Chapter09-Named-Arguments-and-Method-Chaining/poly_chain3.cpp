8b7c49f9b262169c5456267a0b7f4024c740f790
Hans-J. Schmid <knock@myopendoor.de>
2019-07-12 10:25:10 +0200

    plays around with code from chapter 9.

--- Chapter09-Named-Arguments-and-Method-Chaining/poly_chain3.cpp
+++ Chapter09-Named-Arguments-and-Method-Chaining/poly_chain3.cpp
@@ -3,24 +3,49 @@
 using std::cout;
 using std::endl;
 
+// ----------------------------------------------------------------------------- 
 template <typename T>
-class Collection {
-    public:
+class Collection
+{
+public:
     Collection() {}
-    T filter() { cout << "Collection::filter" << endl; return *static_cast<T*>(this); }
-    T sort() { cout << "Collection::sort" << endl; return T(*this); }
+
+    T filter()
+    {
+        cout << "Collection::filter" << endl;
+        return *static_cast<T *>(this);
+    }
+
+    T sort()
+    {
+        cout << "Collection::sort" << endl;
+        return T(*this);
+    }
 };
 
-class SortedCollection : public Collection<SortedCollection> {
-    public:
+class SortedCollection : public Collection<SortedCollection>
+{
+public:
     SortedCollection() {}
-    SortedCollection(const Collection&) {}
-    SortedCollection search() { cout << "SortedCollection::search" << endl; return *this; }
-    SortedCollection median() { cout << "SortedCollection::median" << endl; return *this; }
+    SortedCollection(const Collection &) {}
+
+    SortedCollection search()
+    {
+        cout << "SortedCollection::search" << endl;
+        return *this;
+    }
+
+    SortedCollection median()
+    {
+        cout << "SortedCollection::median" << endl;
+        return *this;
+    }
 };
 
-
-int main() {
-    Collection<SortedCollection> c;
+// ----------------------------------------------------------------------------- 
+int main()
+{
+    // Collection<SortedCollection> c;
+    SortedCollection c;
     c.sort().search().filter().median();
 }
